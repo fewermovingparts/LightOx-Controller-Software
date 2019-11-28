1233078b53cc791504111dd13e6b5d1415e5ab1c
Ian Martin <ian.martin@cantab.net>
2019-11-28 21:16:56 +0000

    Hide warnings from library header files
    By using the gcc pragma all follwoign headers get treated as system headers
    where warnings are ignored. Makes actual warnings in our code visible

---
+++ src/SystemHeaders.h
@@ -0,0 +1,34 @@
+#ifndef SYSTEMHEADERS_H
+#define SYSTEMHEADERS_H
+
+#include <Arduino.h>
+
+#ifdef __GNUC__
+// Avoid tons of warnings from library code
+#pragma GCC system_header
+#endif
+
+
+
+#include <BlockDriver.h>
+#include <FreeStack.h>
+#include <MinimumSerial.h>
+#include <SdFat.h>  //SD card library
+#include <SdFatConfig.h>
+#include <SysCall.h>
+#include <sdios.h>
+
+#include <FT_NHD_43CTP_SHIELD.h>
+//#include <FT_VM801B43.h>
+//#include <FT_VM801P43_50.h>
+//#include "FT_NHD_43RTP_SHIELD.h"  //new screen
+#include <DallasTemperature.h>
+#include <EEPROM.h>
+#include <OneWire.h>  //control devices (from Dallas Semiconductor) that use the One Wire protocol
+#include <SPI.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>  //Allows communication with I2C devices via SDA (data line) and SCL (clock line)
+#include <stdio.h>
+#include "Adafruit_VEML6070.h"  //UV sensor
+
+#endif
